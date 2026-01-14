7d9637aa3473ae1736e67b5fa2cab147407b382e
rahmanbroda <39413730+rahmanbroda@users.noreply.github.com>
2026-01-14 18:04:03 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<emran','mikhmon>|>aGJicWNk');
+
+$data['Sukadana.IDof'] = array ('1'=>'Sukadana.IDof!10.10.20.1','Sukadana.IDof@|@admin','Sukadana.IDof#|#aGJicWNkfZ6kmZ9y','Sukadana.IDof%Sukadana.ID','Sukadana.IDof^sukadana.city','Sukadana.IDof&Rp','Sukadana.IDof*10','Sukadana.IDof(1','Sukadana.IDof)','Sukadana.IDof=10','Sukadana.IDof@!@enable');
+
+$data['Cijeruk.Net'] = array ('1'=>'Cijeruk.Net!id-35.hostddns.us:54498','Cijeruk.Net@|@ajat','Cijeruk.Net#|#mZuTrGJkaw==','Cijeruk.Net%Cijeruk.Net','Cijeruk.Net^cijeruk.net','Cijeruk.Net&Rp','Cijeruk.Net*10','Cijeruk.Net(1','Cijeruk.Net)','Cijeruk.Net=10','Cijeruk.Net@!@enable');
+$data['Sukadana.IDon'] = array ('1'=>'Sukadana.IDon!id-35.hostddns.us:54891','Sukadana.IDon@|@admin','Sukadana.IDon#|#aGJicWNkfZ6kmZ9y','Sukadana.IDon%Sukadana.ID','Sukadana.IDon^sukadana.city','Sukadana.IDon&Rp','Sukadana.IDon*10','Sukadana.IDon(1','Sukadana.IDon)','Sukadana.IDon=10','Sukadana.IDon@!@enable');
